2b06b092bd31f857a12566c40a46d79748aff287
Daniel Felipe Rodríguez Patiño <dfrodriguezp@unal.edu.co>
2019-02-10 00:12:24 -0500

    Finish quadtree class, add query method

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-import quadtree
+from quadtree import Point, Square, Quadtree
 import numpy
 from matplotlib import pyplot
-from matplotlib.patches import Rectangle
 
 l = 10
-sq = quadtree.Square(l/2, l/2, l/2)
-qt = quadtree.QuadTree(sq)
+sq = Square(l/2, l/2, l/2)
+qtree = Quadtree(sq)
 
 points = list()
-for i in range(10):
-    p = quadtree.Point(numpy.random.uniform(l), numpy.random.uniform(l))
+for i in range(100):
+    p = Point(numpy.random.uniform(0, l), numpy.random.uniform(0, l))
     points.append(p)
-    qt.insert(p)
+    qtree.insert(p)
+
+area = Square(3.2, 6, 2.3)
+points_in_area = qtree.query(area)
 
 pyplot.figure()
-qt.show()
 for p in points:
     p.show()
+for p in points_in_area:
+    p.show("red")
+qtree.show()
+area.show("red")
 pyplot.xlim(-0.5, l + 0.5)
 pyplot.ylim(-0.5, l + 0.5)
 pyplot.gca().set_aspect('equal')
--- quadtree.py
+++ quadtree.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as pyplot
+from matplotlib import pyplot
 from matplotlib.patches import Rectangle as rect
 
 class Point(object):
@@ -6,8 +6,8 @@ class Point(object):
         self.x = x
         self.y = y
 
-    def show(self):
-        pyplot.scatter(self.x, self.y)
+    def show(self, color=None):
+        pyplot.scatter(self.x, self.y, color=color)
 
 class Square(object):
     def __init__(self, x, y, halflength):
@@ -16,54 +16,62 @@ class Square(object):
         self.hl = halflength
 
     def containsPoint(self, point):
-        if (point.x < self.x - self.hl or point.y < self.y - self.hl or
-            point.x > self.x + self.hl or point.y > self.y + self.hl):
-            return False
-        return True
+        return not (point.x < self.x - self.hl or point.y < self.y - self.hl or
+            point.x > self.x + self.hl or point.y > self.y + self.hl)
 
-    def show(self):
+    def intersects(self, other):
+        x = self.x - self.hl
+        other_x = other.x - other.hl
+        y = self.y - self.hl
+        other_y = other.y - other.hl
+
+        return ((x <= other_x + other.hl*2 and x >= other_x) or
+            (y <= other_y + other.hl*2 and y >= other_y) or
+            (other_x <= x + self.hl*2 and other_x >= x) or
+            (other_y <= y + self.hl*2 and other_y >= y))
+
+    def show(self, edgecolor=None):
         x, y = self.x - self.hl, self.y - self.hl
-        pyplot.gca().add_patch(rect((x, y), self.hl * 2, self.hl * 2, facecolor='none'))
+        pyplot.gca().add_patch(rect((x, y), self.hl * 2, self.hl * 2, facecolor='none', edgecolor=edgecolor))
 
-        
-
-class QuadTree(object):
+class Quadtree(object):
     def __init__(self, boundary):
         self.boundary = boundary
-        self.capacity = 2
+        self.capacity = 4
         self.points = list()
-        self.northwest = None
-        self.northeast = None
-        self.southwest = None
-        self.southeast = None
-
+        self.divided = False
 
     def subdivide(self):
-        nw_x = self.boundary.x - self.boundary.hl / 2
-        nw_y = self.boundary.y + self.boundary.hl / 2
-        self.northwest = QuadTree(Square(nw_x, nw_y, self.boundary.hl / 2))
+        x = self.boundary.x
+        y = self.boundary.y
+        hl = self.boundary.hl
 
-        ne_x = self.boundary.x + self.boundary.hl / 2
-        ne_y = self.boundary.y + self.boundary.hl / 2
-        self.northeast = QuadTree(Square(ne_x, ne_y, self.boundary.hl / 2))
+        nw_x = x - hl / 2
+        nw_y = y + hl / 2
+        self.northwest = Quadtree(Square(nw_x, nw_y, hl / 2))
 
-        sw_x = self.boundary.x - self.boundary.hl / 2
-        sw_y = self.boundary.y - self.boundary.hl / 2
-        self.southwest = QuadTree(Square(sw_x, sw_y, self.boundary.hl / 2))
+        ne_x = x + hl / 2
+        ne_y = y + hl / 2
+        self.northeast = Quadtree(Square(ne_x, ne_y, hl / 2))
 
-        se_x = self.boundary.x + self.boundary.hl / 2
-        se_y = self.boundary.y - self.boundary.hl / 2
-        self.southeast = QuadTree(Square(se_x, se_y, self.boundary.hl / 2))
+        sw_x = x - hl / 2
+        sw_y = y - hl / 2
+        self.southwest = Quadtree(Square(sw_x, sw_y, hl / 2))
+
+        se_x = x + hl / 2
+        se_y = y - hl / 2
+        self.southeast = Quadtree(Square(se_x, se_y, hl / 2))
+        self.divided = True
 
     def insert(self, point):
         if (not self.boundary.containsPoint(point)):
             return False
 
-        if (len(self.points) < self.capacity and self.northwest == None):
+        if (len(self.points) < self.capacity and not self.divided):
             self.points.append(point)
             return True
 
-        if (self.northwest == None):
+        if (not self.divided):
             self.subdivide()
 
         if (self.northwest.insert(point)):
@@ -77,9 +85,40 @@ class QuadTree(object):
 
         return False
 
+    def query(self, range):
+        points = list()
+        if (not self.boundary.intersects(range)):
+            return points
+
+        for p in self.points:
+            if (range.containsPoint(p)):
+                points.append(p)
+
+        if (not self.divided):
+            return points
+
+        for p in self.northwest.query(range):
+            points.append(p)
+        for p in self.northeast.query(range):
+            points.append(p)
+        for p in self.southwest.query(range):
+            points.append(p)
+        for p in self.southeast.query(range):
+            points.append(p)
+
+        return points
+
+
+
     def show(self):
-        self.boundary.show()
-        self.northwest.boundary.show()
-        self.northeast.boundary.show()
-        self.southwest.boundary.show()
-        self.southeast.boundary.show()
+        x, y = self.boundary.x - self.boundary.hl, self.boundary.y - self.boundary.hl
+        pyplot.gca().add_patch(rect((x, y), self.boundary.hl * 2, self.boundary.hl * 2, facecolor='none'))
+
+        if (self.divided):
+            self.northwest.show()
+            self.northeast.show()
+            self.southwest.show()
+            self.southeast.show()
+
+        # for p in self.points:
+        #     pyplot.scatter(p.x, p.y, color="red")
